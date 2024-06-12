d3186dceb6f0e975b8a9e6f8da36493df90e0370
Kulishevich <maxim8992012@gmail.com>
2024-06-13 00:18:13 +0300

    adaptive 2/2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home: FC = () => {
             </div>
         </div>
         <div className={styles.imageContainer}>
-          <img src="./image15.png" alt="image" className={styles.image}/>
+          <img src="https://s3-alpha-sig.figma.com/img/d8b5/60a4/6936b693bec69faddc38d6d12719ccbe?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BIWo6Mw7CLRZ7LDTryYmcsKz89Cz5C7v~jzBvX1g7HKymJcrB9cAOl2vwNeF1hRRVQ3tHq75j1f~hbR-sW~Uf0eau8vDgbOeEpGvrXNub8wR0ufxFvR5h~Ekr2IbRdu-n3WspO5PQ9e7tcE2ToDLE5wEdAVvw1QxaPQrbDoqFyNpjPgWtbywXnLpgVvYRy0mqm49ejdND9NrA3~MZQOa3NYOT3vakWs3TpTFsXAMLkdcNms3qB2qq7vdx3sHA1UfRziX44iL~L4nuw2ct1qcPNdbvCY3zAdU7u3pFMkF59CtkE90UdOTrrOuGuijigpxo0GztjGDfAWCKyOkiqtLHQ__" alt="image" className={styles.image}/>
         </div>
     </div>
   )
